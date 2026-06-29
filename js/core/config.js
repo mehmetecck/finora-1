23f5f3ee56cb90114d32f2b69b6667c54d407d47
colyst <mehmetecck@gmail.com>
2026-06-29 13:48:33 +0200

    added the public api key for logokit

--- js/core/config.js
+++ js/core/config.js
@@ -2,5 +2,5 @@
    LogoKit uses a publishable token, so it is safe to expose this value in the
    client bundle. Never put a LogoKit secret Brand API token here. */
 window.FINORA_CONFIG = Object.assign({
-  logoKitToken: "YOUR_LOGOKIT_PUBLISHABLE_TOKEN",
+  logoKitToken: "pk_fr277566b689ab2f340988",
 }, window.FINORA_CONFIG || {});
